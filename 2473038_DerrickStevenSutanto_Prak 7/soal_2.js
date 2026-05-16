042caeb5bca909d69ef33f7da943961af7eb81a2
derrickstvn <derrickstevn@gmail.com>
2026-05-16 13:38:00 +0700

    Praktikum 09

--- 2473038_DerrickStevenSutanto_Prak 7/soal_2.js
+++
@@ -1,8 +0,0 @@
-function highlightRow(row) {
-    row.dataset.originalClass = row.className;
-    row.className = "highlight";
-}
-
-function unhighlightRow(row) {
-    row.className = row.dataset.originalClass;
-}
